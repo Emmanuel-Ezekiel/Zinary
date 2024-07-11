84171208f231fb89273944d9ce12b781016b0b96
Emmanuel Ezekiel <emamex44@gmail.com>
2024-07-11 10:02:21 +0100

    Added 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import Wallet from "./routes/wallet/wallets";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./routes/layout";
 import Dashboard from "./routes/dashboard";
+import Loans from "./routes/loans";
+import Transactions from "./routes/Transactions";
+import Audit from "./routes/Audit";
+import Settings from "./routes/Settings";
+import Staff from "./routes/staff";
+import Users from "./routes/users";
+import Payment from "./routes/payment";
+import ErrorPage from "./routes/404";
 
 function App() {
   return (
@@ -10,8 +18,16 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Dashboard />} index />
+          <Route path="/loans" element={<Loans />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/audit-trail" element={<Audit />} />
           <Route path="/wallet" element={<Wallet />} index />
+          <Route path="/settings" element={<Settings />} />
         </Route>
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
---
+++ src/routes/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>Invalid Routes</div>
+  )
+}
+
+export default ErrorPage
